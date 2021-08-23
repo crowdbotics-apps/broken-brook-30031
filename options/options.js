0888e91074470cef0041a4a0f0e58b928ac50a0d
Crowdbotics <team@crowdbotics.com>
2021-08-23 04:06:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "broken_brook_30031Identifier",
+  url: "https://broken_brook_30031Identifier.botics.co",
+  api: "https://broken_brook_30031Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
